3138e146ae988420f3e2e9ae6907672774a8b824
26Ninylichka <gmail.smoljarn@>
2026-02-04 19:57:09 +0100

    Initial commit

---
+++ src/main/java/com/example/cacelator/CacelatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.cacelator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacelatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacelatorApplication.class, args);
+    }
+
+}
